244bf880f0e8ca4caa5f86a8fa2dbd940dd96e55
Claudio Filipi Goncalves dos Santos <claudiofg@MACCIT012710.local>
2015-06-15 08:07:13 -0300

    Initial Commit

---
+++ AOC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AOC
+//
+//  Created by Claudio Filipi Goncalves dos Santos on 6/15/15.
+//  Copyright (c) 2015 Claudio Filipi Goncalves dos Santos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AOCTests/AOCTests.m
@@ -0,0 +1,40 @@
+//
+//  AOCTests.m
+//  AOCTests
+//
+//  Created by Claudio Filipi Goncalves dos Santos on 6/15/15.
+//  Copyright (c) 2015 Claudio Filipi Goncalves dos Santos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface AOCTests : XCTestCase
+
+@end
+
+@implementation AOCTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
